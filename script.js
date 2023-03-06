3eb60fde896b018783e417be3b0de91016ebd51e
Seyi Oyemade <seyioyemade918@gmail.com>
2023-03-06 21:41:01 +0100

    Make mobile menu disappear when the user click (or tap) any of the mobile menu options

--- script.js
+++ script.js
@@ -1,18 +1,19 @@
-const container = document.querySelector('.container');
 const mobileMenuIcons = document.querySelectorAll('nav > button > img');
+const mobileMenu = document.querySelectorAll('.mobile-menu-canvas li');
 const mobileMenuCanvas = document.querySelector('.mobile-menu-canvas');
 
-for (let i = 0; i < mobileMenuIcons.length; i++) {
-  mobileMenuIcons[i].addEventListener('click',function() {
-    container.classList.toggle('show');  
+for (let i = 0; i < mobileMenu.length; i++) {
+  mobileMenu[i].addEventListener('click', () => {
     mobileMenuCanvas.classList.toggle('show');
     mobileMenuIcons[0].classList.toggle('hide');
     mobileMenuIcons[1].classList.toggle('hide');
-
   });
 }
 
-
-
-
-
+for (let i = 0; i < mobileMenuIcons.length; i++) {
+  mobileMenuIcons[i].addEventListener('click', () => {
+    mobileMenuCanvas.classList.toggle('show');
+    mobileMenuIcons[0].classList.toggle('hide');
+    mobileMenuIcons[1].classList.toggle('hide');
+  });
+}
