da88dc95e0b3c0f60c3ac604159098a7f4341a42
SAZZAD1Q2 <kurulia115@gmail.com>
2023-03-07 14:03:12 +0600

    Css file modification to style the website properly

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const mobileMenuIcons = document.querySelectorAll('nav > button > img');
 const mobileMenu = document.querySelectorAll('.mobile-menu-canvas li');
 const mobileMenuCanvas = document.querySelector('.mobile-menu-canvas');
 
-for (let i = 0; i < mobileMenu.length; i++) {
+for (let i = 0; i < mobileMenu.length; i += 1) {
   mobileMenu[i].addEventListener('click', () => {
     mobileMenuCanvas.classList.toggle('show');
     mobileMenuIcons[0].classList.toggle('hide');
@@ -10,7 +10,7 @@ for (let i = 0; i < mobileMenu.length; i++) {
   });
 }
 
-for (let i = 0; i < mobileMenuIcons.length; i++) {
+for (let i = 0; i < mobileMenuIcons.length; i += 1) {
   mobileMenuIcons[i].addEventListener('click', () => {
     mobileMenuCanvas.classList.toggle('show');
     mobileMenuIcons[0].classList.toggle('hide');
